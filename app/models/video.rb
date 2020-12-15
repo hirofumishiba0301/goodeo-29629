b075f0f30da13686f2f2bc38cfaa3fb431a542e6
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-12-15 22:31:47 +0900

    ActiveStrage実装

--- app/models/video.rb
+++ app/models/video.rb
@@ -3,7 +3,7 @@ class Video < ApplicationRecord
   belongs_to :user
   #has_one :samnail
   belongs_to_active_hash :category
-  #has_one_attached :image
+  has_one_attached :image
 
   validates :name, :image, :info, :category, presence: true
   with_options numericality: { other_than: 1 } do
