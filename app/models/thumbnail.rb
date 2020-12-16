7a26b10208f9e5c80f35318519996df6ab5fec4a
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-12-17 00:00:05 +0900

    FORMオブジェクトモデル設定

--- app/models/thumbnail.rb
+++ app/models/thumbnail.rb
@@ -1,3 +1,4 @@
 class Thumbnail < ApplicationRecord
   has_one_attached :poster
+  belongs_to :video
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,12 +1,8 @@
 class Video < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  #has_one :samnail
+  has_one :thumbnail
   belongs_to_active_hash :category
   has_one_attached :image
 
-  validates :name, :image, :info, :category, presence: true
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-  end
 end
