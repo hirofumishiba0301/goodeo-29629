1c3a1eb91a0c31e6e1fae768d76cd889bf9c2299
hirofumishiba0301 <71271771+hirofumishiba0301@users.noreply.github.com>
2020-12-26 00:44:25 +0900

    サムネイル作成編集機能実装

--- app/controllers/thumbnails_controller.rb
+++ app/controllers/thumbnails_controller.rb
@@ -1,2 +1,51 @@
 class ThumbnailsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_thumbnail, only: [:show, :edit, :update, :destroy]
+  before_action :set_video, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @videos = Video.includes(:user).order('created_at DESC')
+  end
+
+  def new
+    @thumbnail = Thumbnail.new
+    #@video_thumbnail = VideoThumbnail.new
+  end
+
+  def create
+    @thumbnail = Thumbnail.new(thumbnail_params)
+    if @thumbnail.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    redirect_to video_path unless @video.user_id == current_user.id
+  end
+
+  def update
+    if @thumbnail.update(thumbnail_params)
+      render :show
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
+  end
+
+
+  def set_thumbnail
+    @thumbnail = Thumbnail.find(params[:id])
+  end
+
+
+  def thumbnail_params
+    params.require(:thumbnail).permit(:poster)
+  end
 end
